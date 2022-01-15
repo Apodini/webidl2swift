835b3e1f5e7a051087a5cd7fa8e4a2bb461be7d8
Max Desiatov <max@desiatov.com>
2022-01-15 21:52:21 +0000

    Switch to Xcode 13.2.1 in `swift.yml` workflow (#25)
    Currently selected version of Xcode makes PRs fail on CI. Let's switch to the latest one. I've also updated SwiftPM dependencies for compatibility with Xcode 13.

--- Package.swift
+++ Package.swift
@@ -8,14 +8,14 @@ let package = Package(
     platforms: [.macOS(.v10_14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .executable(name: "webidl2swift", targets: ["webidl2swift", ]),
+        .executable(name: "webidl2swift", targets: ["webidl2swift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "swift-argument-parser", url: "https://github.com/apple/swift-argument-parser", from: "0.3.1"),
-        .package(name: "swift-format", url: "https://github.com/apple/swift-format.git", .branch("swift-5.3-branch")),
-        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .upToNextMinor(from: "0.50300.0")),
+        .package(name: "swift-argument-parser", url: "https://github.com/apple/swift-argument-parser", from: "1.0.1"),
+        .package(name: "swift-format", url: "https://github.com/apple/swift-format.git", .upToNextMinor(from: "0.50500.0")),
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .upToNextMinor(from: "0.50500.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -29,14 +29,14 @@ let package = Package(
                 .unsafeFlags([
                     // Fix for missing rpath for lib_InternalSwiftSyntaxParser.dylib when building from within Xcode.
                     // lib_InternalSwiftSyntaxParser.dylib is linked by SwiftSyntax.
-                    "-Xlinker", "-rpath", "-Xlinker", "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/"
-                ])
+                    "-Xlinker", "-rpath", "-Xlinker", "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/",
+                ]),
             ]
         ),
         .target(
             name: "WebIDL",
             dependencies: [
-                "SwiftSyntax"
+                "SwiftSyntax",
             ],
             path: "Sources/WebIDL"
         ),
@@ -54,8 +54,8 @@ let package = Package(
                 .unsafeFlags([
                     // Fix for missing rpath for lib_InternalSwiftSyntaxParser.dylib when building from within Xcode.
                     // lib_InternalSwiftSyntaxParser.dylib is linked by SwiftSyntax.
-                    "-Xlinker", "-rpath", "-Xlinker", "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/"
-                ])
+                    "-Xlinker", "-rpath", "-Xlinker", "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/",
+                ]),
             ]
         ),
         .testTarget(
@@ -67,8 +67,8 @@ let package = Package(
                 .unsafeFlags([
                     // Fix for missing rpath for lib_InternalSwiftSyntaxParser.dylib when building from within Xcode.
                     // lib_InternalSwiftSyntaxParser.dylib is linked by SwiftSyntax.
-                    "-Xlinker", "-rpath", "-Xlinker", "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/"
-                ])
+                    "-Xlinker", "-rpath", "-Xlinker", "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/",
+                ]),
             ]
         ),
     ]
