a401fd759446b319b3fda3a546b8793fefeed0f1
Jed Fox <git@jedfox.com>
2022-03-29 10:40:00 -0400

    Update get-webidl.js to ESM to fix runtime error

--- Example/get-webidl.js
+++ Example/get-webidl.js
@@ -1,13 +1,13 @@
-const cheerio = require('cheerio')
-const fetch = require('node-fetch')
-const fs = require('fs')
+import { load } from 'cheerio'
+import fetch from 'node-fetch'
+import { writeFileSync } from 'node:fs'
 
 fetch(`https://${process.argv[2]}.spec.whatwg.org`)
   .then(res => res.text())
-  .then(html => cheerio.load(html))
+  .then(html => load(html))
   .then($ => $('.idl.def, pre code.idl').text())
   .then(idl => {
-    fs.writeFileSync(`WebIDL-files/${process.argv[2]}.webidl`, idl)
+    writeFileSync(`WebIDL-files/${process.argv[2]}.webidl`, idl)
     console.log(`Wrote ${idl.split('\n').length.toLocaleString()} lines!`)
   })
   .catch(err => console.error("Failed!", err))
